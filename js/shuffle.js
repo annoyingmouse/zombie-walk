9d6861c70cf108868f3d9c4cad3d83979d5dec08
Dominic Myers <annoyingmouse@gmail.com>
2022-05-21 14:57:47 +0100

    Working, but could be better!

--- js/shuffle.js
+++ js/shuffle.js
@@ -1,82 +1,49 @@
+
+
+
+
 (() => {
 
-  // const checkIfImageExists = (url, callback) => {
-  //   const img = new Image();
-  //   img.src = url;
-  //   if (img.complete) {
-  //     callback(true);
-  //   } else {
-  //     img.onload = () => {
-  //       callback(true);
-  //     };
-  //
-  //     img.onerror = () => {
-  //       callback(false);
-  //     };
-  //   }
-  // }
-  //
-  const populateImageArray = (src) => {
-    let index = 1
-    let imageSrc = ''
-    let success = true
-    while(success){
-      imageSrc = `${src}/frame_${(index).toString().padStart(2, '0')}.png`
-      console.log(index, imageSrc)
-      if(index > 10){
-        success = false
-      }else {
-        index += 1
-      }
+  const populateImageArray = src => Array.from({length: 8}, (_, i) => i + 1).map(n => `${src}/frame_${(n).toString().padStart(2, '0')}.png`)
+  const canvas = document.getElementById("canvas");
+  const context = canvas.getContext('2d');
+  let images
+
+  const preloadImages = srcs => {
+    const loadImage = src => new Promise(function(resolve, reject) {
+      const img = new Image();
+      img.onload = () => resolve(img)
+      img.onerror = img.onabort = () => reject(src)
+      img.src = src
+    })
+    const promises = [];
+    for (let i = 0; i < srcs.length; i++) {
+      promises.push(loadImage(srcs[i]));
     }
+    return Promise.all(promises);
+  }
 
+  preloadImages(populateImageArray('../images/zombie_01')).then(function(imgArr) {
+    images = imgArr
+    setInterval(shuffle, 150);
+  }, error => {
+    console.log('Error:', error)
+  })
 
-  //   const arr = []
-  //   let index = 1
-  //   let success = true
-  //   let imageSrc = ''
-  //   while(success){
-  //     imageSrc = `${src}/frame_${(index).toString().padStart(2, '0')}.png`
-  //     debugger
-  //     checkIfImageExists(imageSrc, exists => {
-  //       success = false
-  //       if (exists) {
-  //         let image = new Image();
-  //         image.src = `${src}/frame_${(index).toString().padStart(2, '0')}.png`
-  //         arr.push(image)
-  //         index += 1
-  //       } else {
-  //         success = false
-  //       }
-  //     });
-  //   }
-  //   return arr
+  let index = 0
+  let x = -27
+  const steps = [2,2,3,7,6,1,1,1]
+
+  function shuffle() {
+    context.fillStyle = 'white';
+    context.fillRect(0,0,canvas.width,canvas.height);
+    context.drawImage(images[index % images.length], x, 0)
+    index += 1
+    x += steps[(index % images.length)]
+    if(x === 580){
+      clearInterval(shuffle)
+    }
   }
 
 
-  // function addImageProcess(src) {
-  //   return new Promise((resolve, reject) => {
-  //     let img = new Image()
-  //     img.onload = () => resolve(img.height)
-  //     img.onerror = reject
-  //     img.src = src
-  //   })
-  // }
-  //
-  // tmp.addImageProcess(imageUrl).then(height => {
-  //   console.log(height)
-  // })
-
-
-  const canvas = document.getElementById("canvas");
-  const context = canvas.getContext('2d');
-  const images = populateImageArray('../images/zombie_01')
-  //console.log(images)
-
-
-  // const image = new Image();
-  // image.onload = function(){
-  //   context.drawImage(image,0,0);
-  // };
-  // image.src = "../images/zombie_01/frame_01.png";
 })()
