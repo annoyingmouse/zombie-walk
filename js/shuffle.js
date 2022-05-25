9a2c28fdbedf0e4205fb0794ecc840b449cf5a16
Dominic Myers <annoyingmouse@gmail.com>
2022-05-25 18:01:49 +0100

    Shuffling

--- js/shuffle.js
+++ js/shuffle.js
@@ -1,10 +1,10 @@
 class Zombie {
-  constructor(images, x, y){
+  constructor(images, x, y, steps){
     this.images = images
     this.step = this.getRandomInt(0, images.length)
     this.x = this.getRandomInt(x[0], x[1])
     this.y = this.getRandomInt(y[0], y[1])
-    this.steps = [2, 2, 3, 7, 6, 1, 1, 1]
+    this.steps = steps
   }
   getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min
@@ -25,7 +25,7 @@ class Zombie {
   const zombies = []
   const zombiesNum = 120
 
-  const populateImageArray = src => Array.from({length: 8}, (_, i) => i + 1)
+  const populateImageArray = (src, num) => Array.from({length: num}, (_, i) => i + 1)
     .map(n => `${src}/frame_${(n).toString().padStart(2, '0')}.png`)
   const canvas = document.getElementById("canvas");
   const context = canvas.getContext('2d');
@@ -45,10 +45,35 @@ class Zombie {
     return Promise.all(promises);
   }
 
-  preloadImages(populateImageArray('../images/zombie_01')).then(function(imgArr) {
+  // preloadImages(populateImageArray('../images/zombie_01', 8)).then(function(imgArr) {
+  //   images = imgArr
+  //   for(let i = 0; i < zombiesNum; i++){
+  //     zombies.push(new Zombie(images, [-1000, -100], [-100, 100], [2, 2, 3, 7, 6, 1, 1, 1]))
+  //   }
+  //   zombies.sort((a, b) => a.y - b.y)
+  //   const shuffle = setInterval(function(){
+  //     context.fillStyle = 'white';
+  //     context.fillRect(0,0,canvas.width,canvas.height);
+  //     zombies.forEach((zombie, index) => {
+  //       const frame = zombie.getFrame()
+  //       context.drawImage(frame.image, frame.x, frame.y)
+  //       if(frame.x > 600){
+  //         zombies.splice(index, 1)
+  //       }
+  //
+  //     })
+  //     if(zombies.length === 0){
+  //       clearInterval(shuffle)
+  //     }
+  //   }, 100)
+  // }, error => {
+  //   console.warn('Error:', error)
+  // })
+
+  preloadImages(populateImageArray('../images/zombie_02', 4)).then(function(imgArr) {
     images = imgArr
-    for(let i = 9; i < zombiesNum; i++){
-      zombies.push(new Zombie(images, [-1000, -100], [-100, 100]))
+    for(let i = 4; i < zombiesNum; i++){
+      zombies.push(new Zombie(images, [10, 10], [0, 0], [1, 1, 1, 1]))
     }
     zombies.sort((a, b) => a.y - b.y)
     const shuffle = setInterval(function(){
